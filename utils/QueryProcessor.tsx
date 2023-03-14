6edc119f2a5668e66c7732ab30708ec2dae99c86
liam-gersten <gerstenliam@gmail.com>
2023-03-14 16:03:47 -0400

    Which of the following numbers is the largest fix

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,6 +7,20 @@ export default function QueryProcessor(query: string): string {
     );
   } else if (query == "What is your name?") {
     return ("Liam-Jack")
+  } else if (query.startsWith("Which of the following numbers is the")) {
+    if (query.includes("largest")) {
+      var prefix: string = "Which of the following numbers is the largest: "
+      var newQuery: string = query[prefix.length, -1]
+      var numbers: string[] = newQuery.split(", ", 3)
+      var numberArray = [];
+      for (var i = 0; i < numbers.length; i++)
+  
+          numberArray.push(parseInt(numbers[i]));
+
+      return (String(Math.max(...numberArray)))
+    } else {
+      return ""
+    }
   }
   return "";
 }
